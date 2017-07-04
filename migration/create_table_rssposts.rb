13417609a6ad2be17ac14833ad62bef406e0ace2
sheng wei qiu <shadow3x3x3@gmail.com>
2017-07-04 15:23:54 +0800

    Add link column

--- migration/create_table_rssposts.rb
+++ migration/create_table_rssposts.rb
@@ -6,6 +6,7 @@ DB.create_table :rss_posts do
   primary_key :id
   String :title
   text :content
+  String :link
   String :img_url
   timestamp :created_at, :default => Sequel::CURRENT_TIMESTAMP
 end
